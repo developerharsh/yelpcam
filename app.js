ade956eb915c17f8ae04241c2f4b1f1fbf1965f7
developerharsh <harshchauhan880@gmail.com>
2018-06-17 20:18:55 +0530

    comments functionality added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app=express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose"),
 Campground = require("./models/campground"),
+Comment = require("./models/comment"),
 seedDB = require("./seeds");
 
 seedDB();
@@ -42,7 +43,7 @@ app.get("/campgrounds",function(req,res){
             console.log(err);
         }
         else{
-            res.render("index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
         }
     });
     
@@ -73,7 +74,7 @@ app.post("/campgrounds",function(req,res){
 });
 
 app.get("/campgrounds/new",function(req,res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 
@@ -86,7 +87,50 @@ app.get("/campgrounds/:id",function(req,res){
         }
         else
         {
-            res.render("show",{campground: foundCampground});
+            res.render("campgrounds/show",{campground: foundCampground});
+        }
+    })
+    
+});
+
+//=================================
+// Comments Routes
+//=================================
+
+app.get("/campgrounds/:id/comments/new",function(req,res){
+
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new",{campground: campground});
+        }
+    })
+    
+});
+
+app.post("/campgrounds/:id/comments",function(req,res){
+
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save(function(err,camp){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            res.redirect("/campgrounds/"+campground._id);                                           
+                        }
+                    })
+                }
+            })
+            
         }
     })
     
