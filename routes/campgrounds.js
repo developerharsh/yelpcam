284b3a1ceaeca6ca4e9c6416abc18f16fabcf40a
developerharsh <harshchauhan880@gmail.com>
2018-07-15 23:58:09 +0530

    code refrector 3

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,8 +23,12 @@ router.post("/",function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var author= {
+        id:req.user._id,
+        username:req.user.username
+    };
 
-    var newCampground = {name:name, img:image, description: desc};
+    var newCampground = {name:name, img:image, description: desc, author: author};
 
     Campground.create(newCampground,function(err,newCampground){
         if(err)
@@ -41,12 +45,12 @@ router.post("/",function(req,res){
     
 });
 
-router.get("/new",function(req,res){
+router.get("/new",isLoggedIn,function(req,res){
     res.render("campgrounds/new");
 });
 
 
-router.get("/:id",function(req,res){
+router.get("/:id",isLoggedIn,function(req,res){
 
     Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
         if(err)
@@ -66,5 +70,7 @@ function isLoggedIn(req,res,next){
     }
     res.redirect("/login");
 }
+
+
 
 module.exports = router;
