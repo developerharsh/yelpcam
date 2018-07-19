d1cc20e56de792a297610cd6733930ae35278027
developerharsh <harshchauhan880@gmail.com>
2018-07-20 00:47:17 +0530

    dynamic pricing done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,13 +24,14 @@ router.post("/",function(req,res){
    
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     var desc = req.body.description;
     var author= {
         id:req.user._id,
         username:req.user.username
     };
 
-    var newCampground = {name:name, img:image, description: desc, author: author};
+    var newCampground = {name:name, img:image, description: desc, author: author, price:price};
 
     Campground.create(newCampground,function(err,newCampground){
         if(err)
